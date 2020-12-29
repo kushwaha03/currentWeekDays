d11f81fdce4c0ada100b950eb3648fad094be460
krishna <33350985+kushwaha03@users.noreply.github.com>
2020-12-29 19:53:24 +0530

    Create currentWeekDay.php

---
+++ currentWeekDay.php
@@ -0,0 +1,16 @@
+                            <!-- <?php
+                              $week=53;
+                              $year=2020;
+                              $date = \Carbon\Carbon::now();
+                              $date->setISODate($year,$week);
+                              // echo $date->startOfWeek()->format('m/d');
+                              // echo $date->endOfWeek();
+                              $mon = \Carbon\Carbon::now()->startOfWeek();
+                              $tue = $mon->copy()->addDay();
+                              $wed = $tue->copy()->addDay();
+                              $thu = $wed->copy()->addDay();
+                              $fri = $thu->copy()->addDay();
+                              $sat = $fri->copy()->addDay();
+                              $sun = $sat->copy()->addDay();
+                              echo $mon->format('m/d'),$tue->format('m/d'),$wed->format('m/d'), $thu->format('m/d'), $fri->format('m/d'), $sat->format('m/d'), $sun->format('m/d');
+                              ?> -->
